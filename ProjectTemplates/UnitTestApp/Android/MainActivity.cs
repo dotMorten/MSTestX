126259752ea090e558ca7af365c60ac82404bfc8
Morten Nielsen <no@spam.thx>
2018-08-14 22:16:20 -0700

    Added project template

---
+++ ProjectTemplates/UnitTestApp/Android/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace $ext_safeprojectname$.Droid
+{
+    [Activity(Label = "UnitTestsA", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            var testOptions = new MSTestX.TestOptions();
+            // You can deploy and launch the app from the ADB shell and parsing intent parameters
+            // Example:
+            // adb install PATH_TO_APK/TestAppRunner.Android-Signed.apk
+            // Launch the app and pass parameters where -n is for starting an intent
+            // The intent should be [AppName]/[Activity Name]
+            // Use --ez for passing boolean, --es for passing a string, --ei for passing int
+            // adb shell am start -n TestAppRunner/TestAppRunner.Android --ez AutoRun true --es TrxReportFile TestReport
+            // Once test run is complete you can copy the report back:
+            // adb exec -out run -as com.mstestx.TestAppRunner cat/data/data/com.mstestx.TestAppRunner/files/TestReport.trx > TestReport.trx
+            testOptions.AutoRun = Intent.GetBooleanExtra("AutoRun", false);
+            string path = Intent.GetStringExtra("ReportFile");
+            // Or generate a new time-stamped log file path on each run:
+            // if (string.IsNullOrEmpty(path))
+            //     path = "TestAppRunner_" + System.DateTime.Now.ToString("yyyy_MM_dd_HH-mm-ss");
+
+            if (!string.IsNullOrEmpty(path))
+            {
+                path = System.IO.Path.Combine(ApplicationContext.FilesDir.Path, path);
+                testOptions.TrxOutputPath = path + ".trx";
+                testOptions.ProgressLogPath = path + ".log";
+            }
+            testOptions.TerminateAfterExecution = testOptions.AutoRun;
+            var testApp = new MSTestX.RunnerApp(testOptions);
+
+            // Disable screen saver while tests are running
+            testApp.TestRunStarted += (a, testCases) => Window?.AddFlags(WindowManagerFlags.KeepScreenOn);
+            testApp.TestRunCompleted += (a, results) => Window?.ClearFlags(WindowManagerFlags.KeepScreenOn);
+
+            LoadApplication(testApp);
+        }
+    }
+}
+
---
+++ ProjectTemplates/UnitTestApp/iOS/Main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace $ext_safeprojectname$.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        static void Main(string[] args)
+        {
+            var procArgs = NSProcessInfo.ProcessInfo.Arguments;
+            // Parse arguments and set up test options based on this.
+            Dictionary<string, string> arguments = new Dictionary<string, string>();
+            if (procArgs.Count() > 0)
+            {
+                for (int i = 1; i < procArgs.Length; i++)
+                {
+                    var a = procArgs[i];
+                    if (a.StartsWith("-"))
+                    {
+                        string value = null;
+                        if (i + 1 < procArgs.Length)
+                        {
+                            var nextArg = procArgs[i + 1];
+                            if (!nextArg.StartsWith("-"))
+                            {
+                                value = nextArg;
+                                i++;
+                            }
+                        }
+                        arguments[a] = value;
+                        System.Diagnostics.Debug.WriteLine($"Argument: {a} = '{value}'");
+                    }
+                }
+            }
+            if (arguments.ContainsKey("AutoRun") && bool.TryParse(arguments["AutoRun"], out bool result))
+            {
+                TestOptions.AutoRun = result;
+                TestOptions.TerminateAfterExecution = result;
+            }
+            if (arguments.ContainsKey("ReportFile"))
+            {
+                TestOptions.TrxOutputPath = arguments["ReportFile"] + ".trx";
+                TestOptions.ProgressLogPath = arguments["ReportFile"] + ".log";
+            }
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+        public static MSTestX.TestOptions TestOptions { get; } = new MSTestX.TestOptions();
+    }
+}
