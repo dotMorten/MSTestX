435547337f4ad0a6b57720b5157f6f7662777c35
dotMorten <mn@iter.dk>
2019-04-30 00:41:37 -0700

    Console runner v0.16.1

--- Automation/MSTestX.Console/Properties/AssemblyInfo.cs
+++ Automation/MSTestX.Console/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MSTestX.Android.ADB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MSTestX.Console")]
+[assembly: AssemblyDescription("Commandline tool for deploying, running and monitoring MSTestX Unit Tests on Android and iOS devices, and generating TRX TestResult reports")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MSTestX.Android.ADB")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("MSTestX.Console")]
+[assembly: AssemblyCopyright("Copyright © 2019 Morten Nielsen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.16.1.0")]
+[assembly: AssemblyFileVersion("0.16.1.0")]
