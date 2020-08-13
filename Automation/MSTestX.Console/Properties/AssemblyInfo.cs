71a63f10a1a67142cca1f0320eab9dcc9fd9add2
Morten <mn@iter.dk>
2020-08-12 21:41:47 -0700

    Update (C)

--- Automation/MSTestX.Console/Properties/AssemblyInfo.cs
+++ Automation/MSTestX.Console/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MSTestX.Console")]
-[assembly: AssemblyCopyright("Copyright © 2019 Morten Nielsen")]
+[assembly: AssemblyCopyright("Copyright © 2019-2020 Morten Nielsen")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
