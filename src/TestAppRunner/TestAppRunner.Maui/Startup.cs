483fec18cd07df8fe19ac9fd96e433f92172e181
Morten Nielsen <mn@iter.dk>
2021-08-25 16:34:16 -0700

    Added Maui test runner app

---
+++ src/TestAppRunner/TestAppRunner.Maui/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Hosting;
+using Microsoft.Maui.Controls.Compatibility;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Controls.Xaml;
+using MSTestX;
+[assembly: XamlCompilationAttribute(XamlCompilationOptions.Compile)]
+
+namespace TestAppRunner.Maui
+{
+	public class Startup : IStartup
+	{
+		public void Configure(IAppHostBuilder appBuilder)
+		{
+			appBuilder
+				.UseMauiApp<RunnerApp>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
+			var preserve = typeof(UnitTests.AttachmentTests).Assembly;
+		}
+	}
+}
