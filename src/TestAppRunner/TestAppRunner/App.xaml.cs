b3802629b620bb05d67f73f24c22d6f3e28d2b99
Morten Nielsen <mn@iter.dk>
2022-04-20 10:59:27 -0700

    Updated MAUI support to RC1

--- src/TestAppRunner/TestAppRunner/App.xaml.cs
+++ src/TestAppRunner/TestAppRunner/App.xaml.cs
@@ -47,8 +47,8 @@ namespace MSTestX
 
 #if MAUI
 #if __ANDROID__
-            TestRunStarted += (a, testCases) => { Dispatcher.Dispatch(() => Microsoft.Maui.Essentials.Platform.CurrentActivity.Window?.AddFlags(Android.Views.WindowManagerFlags.KeepScreenOn)); };
-            TestRunCompleted += (a, results) => { Dispatcher.Dispatch(() => Microsoft.Maui.Essentials.Platform.CurrentActivity.Window?.ClearFlags(Android.Views.WindowManagerFlags.KeepScreenOn)); };
+            TestRunStarted += (a, testCases) => { Dispatcher.Dispatch(() => Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.Window?.AddFlags(Android.Views.WindowManagerFlags.KeepScreenOn)); };
+            TestRunCompleted += (a, results) => { Dispatcher.Dispatch(() => Microsoft.Maui.ApplicationModel.Platform.CurrentActivity.Window?.ClearFlags(Android.Views.WindowManagerFlags.KeepScreenOn)); };
 #elif __IOS__
             TestRunStarted += (a, testCases) => { Dispatcher.Dispatch(() => UIKit.UIApplication.SharedApplication.IdleTimerDisabled = true); };
             TestRunCompleted += (a, results) => { Dispatcher.Dispatch(() => UIKit.UIApplication.SharedApplication.IdleTimerDisabled = false); };
